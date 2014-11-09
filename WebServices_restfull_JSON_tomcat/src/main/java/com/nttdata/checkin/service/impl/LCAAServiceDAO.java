08af78aa166504baa7476e90ebf31adc5c387c5c
pham tuan <phamtuanchip@gmail.com>
2014-11-10 00:58:41 +0700

    new branch with change source

---
+++ WebServices_restfull_JSON_tomcat/src/main/java/com/nttdata/checkin/service/impl/LCAAServiceDAO.java
@@ -0,0 +1,14 @@
+package com.nttdata.checkin.service.impl;
+
+import com.nttdata.checkin.modal.User;
+import com.nttdata.checkin.service.DSCService;
+
+public class LCAAServiceDAO implements DSCService{
+
+	@Override
+	public User login(User u) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
