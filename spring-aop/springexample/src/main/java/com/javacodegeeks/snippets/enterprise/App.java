dec7dd554c47e2ba93df49988a991dcf1ef85dcf
Pham Tuan <phamtuanchip@gmail.com>
2015-12-09 14:36:03 +0700

    add example spring AOP

---
+++ spring-aop/springexample/src/main/java/com/javacodegeeks/snippets/enterprise/App.java
@@ -0,0 +1,23 @@
+package com.javacodegeeks.snippets.enterprise;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+			ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			SimpleService simpleService = (SimpleService) context.getBean("simpleServiceBean");
+			simpleService.printNameId();
+			System.out.println("--------------");
+			try{
+				simpleService.checkName();
+			} catch(Exception e){
+				System.out.println("SimpleService: Method checkName() exception thrown..");
+			}
+			System.out.println("--------------");
+			simpleService.sayHello("Javacodegeeks");
+			context.close();
+	}
+}
